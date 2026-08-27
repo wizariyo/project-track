efd3623308a984f05b2756a975f572e6d784613e
wizariyo <yathaarth.bhardwaj@gmail.com>
2026-08-27 20:49:16 +0530

    Update server and styling

---
+++ server.js
@@ -0,0 +1,26 @@
+﻿const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const mime = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif'
+};
+const server = http.createServer((req, res) => {
+  let urlPath = req.url.split('?')[0];
+  let filepath = path.join(__dirname, urlPath === '/' ? 'index.html' : urlPath);
+  const ext = path.extname(filepath);
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Not Found");
+    } else {
+      res.writeHead(200, { 'Content-Type': mime[ext] || 'text/plain' });
+      res.end(data);
+    }
+  });
+});
+server.listen(8080, () => console.log('Server running on 8080'));
